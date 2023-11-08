89cf94a15d663aee65a5875e8de853e2e756f0c5
Eder <ederfornero.ef@gmail.com>
2023-11-08 06:37:09 -0300

    model,route,controller

---
+++ controllers/url.ts
@@ -0,0 +1,3 @@
+const newUrl = async (req, res) => {
+  
+}
---
+++ models/url.ts
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const urlSchema = new mongoose.Schema({
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  redirectUrl: {
+    type: String,
+    require: true
+  },
+  visitAtUrl: [
+    {
+      timestamp:
+        { type: Number }
+    }
+  ],
+  },
+  {
+    timestamps: true
+  }
+)
+
+const url = mongoose.model("url", urlSchema)
+module.exports = URL; 
